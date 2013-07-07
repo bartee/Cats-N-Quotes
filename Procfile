cb6fe4709d3b13cd24df20319fd3fded55ed2b79
bartee <bart.stroeken@gmail.com>
2013-07-07 11:59:30 +0200

    And again, another move

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn random_quote.wsgi:application
--- quotes/admin.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'bartstroeken'
-
-from django.contrib import admin
-from quotes.models import Quote
-
-class QuoteAdmin(admin.ModelAdmin):
-    list_display = ['quote', 'comeback','avatar_html']
-
-admin.site.register(Quote, QuoteAdmin)
