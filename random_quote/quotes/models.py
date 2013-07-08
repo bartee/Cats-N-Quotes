9fa330a1ea17fdf51b88e9393adb9dadf02211da
bartee <bart.stroeken@gmail.com>
2013-07-08 15:40:07 +0200

    Comment change

--- random_quote/quotes/models.py
+++ random_quote/quotes/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Quote(models.Model):
     quote = models.CharField(max_length=255,help_text='De Quote')
     comeback = models.CharField(max_length=255,blank=True, help_text='Wanneer er een nifty comeback is, voeg hem toe')
-    original_author = models.CharField(max_length=100, help_text='wie heeft hem in het leven geroepen?')
+    original_author = models.CharField(max_length=100, help_text='wie heeft hem in het leven geroepen? Vul zijn/haar email adres in om een Gravatar op te halen')
     pub_date = models.DateTimeField('date published',auto_now_add=True,editable=False)
 
     def __unicode__(self):
