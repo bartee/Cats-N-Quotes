080ab188535d7f5eefdfd2a6df57c067b75531d4
bartee <bart.stroeken@gmail.com>
2013-07-15 09:17:16 +0200

    exit south

--- random_quote/random_quote/settings.py
+++ random_quote/random_quote/settings.py
@@ -122,7 +122,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'quotes',
-    'south',
+#    'south',
     # 'debug_toolbar',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
