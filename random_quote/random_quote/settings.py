7cc8eadffc4ef33c07ca4a623d1309c2a29e820a
bartee <bart.stroeken@gmail.com>
2013-07-17 17:55:54 +0200

    Changed settings for templates and static dir, and removed a stupid modal

--- random_quote/random_quote/settings.py
+++ random_quote/random_quote/settings.py
@@ -1,4 +1,7 @@
 # Django settings for random_quote project.
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -62,7 +65,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_ROOT + '/static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -110,6 +113,7 @@ ROOT_URLCONF = 'random_quote.urls'
 WSGI_APPLICATION = 'random_quote.wsgi.application'
 
 TEMPLATE_DIRS = (
+    PROJECT_ROOT + '/templates',
 )
 
 INSTALLED_APPS = (
