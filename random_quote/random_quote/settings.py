b2a26556a25048d4af16f8e02e6348aa5f57c96d
Bart Stroeken <bart.stroeken@gmail.com>
2013-12-08 09:02:18 +0100

    Updated the uploads-path

--- random_quote/random_quote/settings.py
+++ random_quote/random_quote/settings.py
@@ -49,7 +49,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/Development/catsnquotes/venv/Cats-N-Quotes/media/'
+MEDIA_ROOT = PROJECT_ROOT + '/uploads/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
