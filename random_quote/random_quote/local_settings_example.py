431faf3178f7d2472f22ee220af28a119aa7e710
Fabio Bier <fab.bier@gmail.com>
2016-05-12 10:51:48 +0200

    Added local_settings_example.py.

---
+++ random_quote/random_quote/local_settings_example.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'random_quote',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
